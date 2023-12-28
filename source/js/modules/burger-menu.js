e42937dd17d9a00a4cb8b9e72ff62516f8e103b8
NadezhdaObsholova <obsholova.n@gmail.com>
2023-12-28 03:55:33 +0300

    different widht

--- source/js/modules/burger-menu.js
+++ source/js/modules/burger-menu.js
@@ -1,19 +1,29 @@
-//BURGER-MENU
+
 const burgerMenu = function () {
   const menuElement = document.querySelector('.main-nav__wrapper');
   const menuButtonElement = document.querySelector('.main-nav__toggle');
   const headerNavElement = document.querySelector('.main-nav__list');
   const menuLogoElement = document.querySelector('.main-nav__logo-link');
+  const bodyElement = document.querySelector('body');
+  const mainNavItems = document.querySelectorAll('.main-nav__item');
 
-  const onClickMenuButtonElement = () => {
+  const toggleMenu = () => {
     menuElement.classList.toggle('main-nav__wrapper--open');
     headerNavElement.classList.toggle('main-nav__list--open');
     menuButtonElement.classList.toggle('main-nav__toggle-close');
     menuLogoElement.classList.toggle('main-nav__logo-link--menu-open');
+    bodyElement.classList.toggle('is-main-nav-open');
   };
 
-  menuButtonElement.addEventListener('click', onClickMenuButtonElement);
+  menuButtonElement.addEventListener('click', () => {
+    toggleMenu();
+  });
+
+  mainNavItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      toggleMenu();
+    });
+  });
 };
 
-export {burgerMenu};
-
+export { burgerMenu };
